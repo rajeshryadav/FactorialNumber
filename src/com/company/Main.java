377ce3b6b823c070b14b33abb5c2706bb23d7619
Rahul kumar <rahulkumar96369@gmail.com>
2019-09-25 19:27:41 +0530

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int number= 7;
+        int factor= 1;
+
+        for(int i=1;i<=7; i++)
+            factor=factor*i;
+
+        System.out.println(factor);
+
+    }
+}
